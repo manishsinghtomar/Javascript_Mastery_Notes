edf0c2b84a5efcfae746450f6dd63f8399269785
Manish Singh Tomar <manishtomar00mt@gmail.com>
2023-09-03 07:03:01 +0530

    index11.js modified

--- index11.js
+++ index11.js
@@ -1,4 +1,4 @@
-//callback functions
+//callback function:A callback is a function passed as an argument to another function
 
 function myFunc2(name){
     console.log("inside my func 2");
@@ -6,8 +6,42 @@ function myFunc2(name){
 }
 
 function myFunc(callback){
-    console.log("hello there i am a func and i can.."
-    callback();
+    console.log("hello there i am a func and i can..");
+    callback("manish");
 }
 
 myFunc(myFunc2);
+
+//---------------------------------------------------------//
+
+//function returning function 
+
+function myFunction(){
+     function hello(){
+        return "hello world"
+     }
+     return hello;
+}
+
+const ans = myFunction();
+console.log(ans());
+// here , returned hello function will  store in ans variable and then ans variable become function whenever we will call ans(), hello function will be invoked automatically  
+
+
+
+//Whenever a function accepts another function as an input or return function, or in both cases, that function becomes a high-order function.
+
+
+//ex-2: Direct function return 
+
+function myFun(){
+    return function(){
+        return "hello manish";
+    };
+}
+
+const answ = myFun();
+console.log(answ());
+
+
+
