d3ad2cc2694628ce044c0a6d4a176c5d3e0436de
MANISH TOMAR <manishtomar00mt@gmail.com>
2023-09-05 17:41:49 +0530

    index18.js modified

---
+++ index18.js
@@ -0,0 +1,16 @@
+//every method
+//  it also take callback as input.
+//it returns boolean values that either be True or false.
+//it returns true if all elements of array satisfies 
+const numbers = [2,4,6,8,10];
+
+function isEven((number)=>number%2 ===0);
+
+const ans = number 
+
+//callback function  --->  true / false (boolean)
+
+//every method ---> true / false (boolean)
+
+console.log(ans)
+
