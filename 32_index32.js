22971b497e8f0884431a5fd73683d24d71e3336e
Manish Singh Tomar <manishtomar00mt@gmail.com>
2023-09-11 23:18:12 +0530

    32_index32.js Added (store methods in different object)

---
+++ 32_index32.js
@@ -0,0 +1,34 @@
+//store methods in different object
+
+const userMethods ={
+    about: function(){
+        return `${this.firstName} is ${this.age} years old and live in ${this.address}`;
+     },
+    is18: function(){
+        return this.age>=18;
+     },
+    sing:function(){
+        return 'tun naa na na la la ';
+    } 
+}
+
+function createUser(firstName , lastName , email, age , address){
+    const user= {};
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    user.about = userMethods.about;
+    user.is18 = userMethods.is18;
+    user.sing = userMethods.sing;
+    return user;
+}
+
+const user1 = createUser('manish','Tomar','manishtomar00mt@gmail.com',23, "delhi");
+const user2 = createUser('Sonal','Mishra','manishtomar00mt@gmail.com',23, "delhi");
+const user3 = createUser('Varun','Tomar','varuntomar3233@gmail.com',18, "delhi");
+
+console.log(user1.about());
+console.log(user2.about());
+console.log(user3.sing());
