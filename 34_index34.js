25dde55091a759a723dfff224e72d200ee0040a1
Manish Singh Tomar <manishtomar00mt@gmail.com>
2023-09-12 00:36:47 +0530

    34_index34.js modified

---
+++ 34_index34.js
@@ -0,0 +1,33 @@
+//important - application of object
+const userMethods ={
+    about: function(){
+        return `${this.firstName} is ${this.age} years old and live in ${this.address}`;
+     },
+    is18: function(){
+        return this.age>=18;
+     },
+    sing:function(){
+        return 'tun naa na na la la ';
+    } 
+}
+
+function createUser(firstName , lastName , email, age , address){
+    const user= Object.create(userMethods);// hence userMethod become the proto of user mean it can use the key value pair of userMethod object.
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    user.about = userMethods.about;
+    user.is18 = userMethods.is18;
+    user.sing = userMethods.sing;
+    return user;
+}
+
+const user1 = createUser('manish','Tomar','manishtomar00mt@gmail.com',23, "delhi");
+const user2 = createUser('Sonal','Mishra','manishtomar00mt@gmail.com',23, "delhi");
+const user3 = createUser('Varun','Tomar','varuntomar3233@gmail.com',18, "delhi");
+
+console.log(user1);
+console.log(user2.about());
+console.log(user3.sing());
