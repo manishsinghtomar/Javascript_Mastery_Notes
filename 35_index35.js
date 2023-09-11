b58370933aa5edc178097e41ab3e51a5f90f74dc
Manish Singh Tomar <manishtomar00mt@gmail.com>
2023-09-12 00:37:04 +0530

    35_index35.js Added

---
+++ 35_index35.js
@@ -0,0 +1,18 @@
+function hello(){
+    console.log("hello world");
+}
+
+// javascript function ===> function + objects
+
+//console.log(hello.name);
+
+//you can add your own properties
+// hello.myOwnProperty = "very unique value";
+// console.log(hello.myOwnProperty);
+
+// name property --> tells function name;
+
+// function provides more useful properties like call , apply and bind etc.
+
+hello.prototype; // {}
+console.log(hello.prototype);
