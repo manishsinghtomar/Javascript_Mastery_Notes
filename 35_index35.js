791fe5cee4a1f35d3654253e6c4a81f2c6113400
MANISH TOMAR <manishtomar00mt@gmail.com>
2023-09-12 23:59:14 +0530

    35_index35.js Added

--- 35_index35.js
+++ 35_index35.js
@@ -2,17 +2,39 @@ function hello(){
     console.log("hello world");
 }
 
-// javascript function ===> function + objects
+// const hello = {key1 : "value1"} //prototype not present
+// const hello = ["value1","value2","value3"] //prototype not present
 
-//console.log(hello.name);
+// javascript function ===> function + objects ( in js, we can treat functions as objects)
+
+console.log(hello.name);//
 
 //you can add your own properties
-// hello.myOwnProperty = "very unique value";
-// console.log(hello.myOwnProperty);
+
+hello.myOwnProperty = "very unique value";
+console.log(hello.myOwnProperty);
 
 // name property --> tells function name;
 
 // function provides more useful properties like call , apply and bind etc.
 
-hello.prototype; // {}
-console.log(hello.prototype);
+//function provides empty object that can be called prototype //{}
+
+
+console.log(hello.prototype); // {} --> free empty object
+
+if(hello.prototype){
+    console.log("prototype is present");
+}else {
+    console.log("prototype is not present");
+}
+
+//only functions provide prototype property  
+
+hello.prototype.abc= "abc";
+hello.prototype.xyz= "xyz";
+hello.prototype.sind = function(){
+    return "lalalla";
+};
+
+console.log(hello.prototype.sind());
