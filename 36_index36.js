c8e41bf4d18344a43a263acf03576f86486c38c3
MANISH TOMAR <manishtomar00mt@gmail.com>
2023-09-13 23:15:35 +0530

    36_index36.js  Added (Use prototype)

---
+++ 36_index36.js
@@ -0,0 +1,30 @@
+// Use prototype
+
+function createUser(firstName , lastName , email, age , address){
+    const user= Object.create(createUser.prototype);// {}
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    return user;
+}
+createUser.prototype.about = function(){
+    return `${this.firstName} is ${this.age} years old and live in ${this.address}`;
+ };
+
+createUser.prototype.is18 = function(){
+    return this.age>=18;
+ };
+createUser.prototype.sing = function(){
+    return 'tun naa na na la la ';
+};
+
+
+const user1 = createUser('manish','Tomar','manishtomar00mt@gmail.com',23, "delhi");
+const user2 = createUser('Sonal','Mishra','manishtomar00mt@gmail.com',23, "delhi");
+const user3 = createUser('Varun','Tomar','varuntomar3233@gmail.com',18, "delhi");
+
+console.log(user1);
+console.log(user2.about());
+// console.log(user3.sing());
