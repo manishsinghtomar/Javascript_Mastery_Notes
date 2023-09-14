2186da59a85f64b0b0c2269c142020e2d9a166c0
Manish Singh Tomar <manishtomar00mt@gmail.com>
2023-09-14 23:42:16 +0530

    37_index37.js Added (new keyword)

--- 37_index37.js
+++ 37_index37.js
@@ -1,5 +1,15 @@
 // new keyword
 function createUser(firstName , age){
     this.firstName = firstName;
-    
+    this.age = age;
 }
+
+const user1 = new createUser("harshit", 6);
+
+// new keyword
+// 1.) first it create empty object --> this = {}
+// 2.) return this 
+// 3.) 
+
+console.log(user1);
+user1.about();
