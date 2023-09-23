c28b3beb9af74e2a67717ff1657167f5f2f658d3
MANISH TOMAR <manishtomar00mt@gmail.com>
2023-09-23 22:07:29 +0530

    File updated (solution using object.create)

--- 33_index33.js
+++ 33_index33.js
@@ -3,35 +3,42 @@
 const obj1 ={
     key1: "value1",
     key2: "value2",
-    
 }
+
+
 // const obj2 ={
 //     key3: "value3",
 // }
 
-// console.log(obj2.key1)
+// console.log(obj2.key1) //undefined
 
 // const obj2 ={};
+// there is one way to create empty object
 // obj2.key3= "value3";
 // console.log(obj2.key3);
 
 //--------------------------------------------------------//
 
 // __proto__ 
+
 // offical ecmascript documentation
+
 // [[prototype]]
+
 // __proto__ or [[prototype]] (both are same)
 
 //when an object is created in javascript, Javascript engine adds a __proto__ property to the newly created object which is called dunder proto or __proto__ 
 
+//--------------------------------------------------------//
+// prototype and dunder proto both are different 
 
-// prototype
-
-const obj2 = Object.create(obj1);//{}
+const obj2 = Object.create(obj1);// it returns empty object or {}
 //there is one more way to create empty object.
 obj2.key3 = "value3";
-// obj1.key2= "unique"
+obj1.key2= "unique";
 console.log(obj2);
+console.log(obj2.key2);
+//javascript phele object me value check kregi agar waha nhi hui toh __proto__ me check kr k value fetch kregi 
 
 console.log(obj2.__proto__);
 
