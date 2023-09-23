7f1961ab2c18d7912e0dbb0375529ef33c75fd61
MANISH TOMAR <manishtomar00mt@gmail.com>
2023-09-23 22:09:46 +0530

    file modifed (Use prototype)

--- 36_index36.js
+++ 36_index36.js
@@ -1,17 +1,53 @@
 // Use prototype
 
-
-
+//-------------------------------------------------------------------------------------
+// const user1 = {
+//     firstName: "Manish",
+//     lastName: "Tomar",
+//     email:"manishtomar00mt@gmail.com",
+//     age: 22,  
+//     address: "House Number, Colony, pincode, state", 
+//     about: function(){
+//         return  `${this.firstName} is ${this.age} years old`    
+//     },
+//     is18: function(){
+//       return this.age >= 18;
+//     }
+// }
+//----------------------------------------------------------------------------------------
+ 
 //function (that funciton creates object)
 //2.) add key value pair
 //3.) object ko return krega
+
+const userMethods ={
+    about: function(){
+        return `${this.firstName} is ${this.age} years old`;
+    },
+    is18: function(){
+        return this.age >=18;
+    }
+}
+
+//-----------------------------------------------------------------------------------------
 function createUser(firstName , lastName , email, age , address){
-    const user= Object.create(createUser.prototype);// {}
+    const user = {}
     user.firstName = firstName;
     user.lastName = lastName;
     user.email = email;
     user.age = age;
     user.address = address;
+// about: function(){
+ //   return  `${this.firstName} is ${this.age} years old`    
+ // },
+ // is18:function(){
+ //   return this.age >= 18;
+ // }
+
+//another way (method separation method)
+user.about = userMethods.about; //reference hai yeh  or  hum address likh rahe hai userMethods k about method ka
+user.is18 = userMethods.is18;
+
     return user;
 }
 createUser.prototype.about = function(){
@@ -26,10 +62,12 @@ createUser.prototype.sing = function(){
 };
 
 
-const user1 = createUser('manish','Tomar','manishtomar00mt@gmail.com',23, "delhi");
-const user2 = createUser('Sonal','Mishra','manishtomar00mt@gmail.com',23, "delhi");
+const user1 = createUser('manish','Tomar','manishtomar00mt@gmail.com',22, "delhi");
+const user2 = createUser('Sonal','Mishra','manishtomar00mt@gmail.com',22, "Bihar");
 const user3 = createUser('Varun','Tomar','varuntomar3233@gmail.com',18, "delhi");
 
 console.log(user1);
 console.log(user2.about());
 // console.log(user3.sing());
+
+
