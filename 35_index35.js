a9e337586487dc86485bdf936cc9b542e32574e9
MANISH TOMAR <manishtomar00mt@gmail.com>
2023-09-23 22:09:14 +0530

    file modified (What is prototype )

--- 35_index35.js
+++ 35_index35.js
@@ -1,9 +1,10 @@
-//what is prototype  
+//what is prototype  {it is a property of function}
+
 //Every object in JavaScript has a built-in property, which is called its prototype. The prototype is itself an object, so the prototype will have its own prototype, making what's called a prototype.
 
-//let hello2 =[1,2,3];
-let hello2 = new array (1,2,3);
-console.log(hello2.prototype);
+// let hello2 =[1,2,3];
+// let hello2 = new array(1,2,3);
+// console.log(hello2.prototype); //only works with function
 
 
 function hello(){
@@ -16,7 +17,7 @@ console.log(hello.prototype);
 
 // javascript function ===> function + objects ( in js, we can treat functions as objects)
 
-console.log(hello.name);//
+console.log(hello.name);// it prints the name of function
 
 //you can add your own properties
 
@@ -49,3 +50,10 @@ hello.prototype.sind = function(){
 console.log(hello.prototype.sind());
 
 //by default array ka prototype array hi hota hai aur object ka prototype object hota hai but we can change  it into another type.
+
+
+// difference of prototype and proto 
+//prototype function ki property hai aur __proto__  ek reference hai jo yeh btata hai agar ek object me value nhi hai to konse object me ja kar check krna hai  
+//prototype ko empty object k trah treat kro 
+//prototype simply ek function ka empty object hota hai 
+//proto type ek reference hai jo chaining krta hai  
