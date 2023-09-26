1e2ab994f4c6c7adbd8cbe1398693c79082cfba6
Manish Singh Tomar <manishtomar00mt@gmail.com>
2023-09-26 10:39:14 +0530

    42_index42.js Added ( getPrototypeof( ))

--- 42_index42.js
+++ 42_index42.js
@@ -1,11 +1,22 @@
-let numbers = [1,2,3];
+let numbers = [1,2,3]; //we use this syntax because it is simple to write.
+
+//javascript ko array k methods milte hai prototype se aur prototype sirf function ka hota hai but "numbers" toh function nhi hai  
+
+//internally javascript array create karne k liye Array constructor call karta hai usko new lga kar call kra sakte hai
+
 // let numbers = new Array(1,2,3); //--> behind the scene
-console.log(Object.getPrototypeOf(numbers));
 // console.log(Array.prototype);
+
+console.log(Object.getPrototypeOf(numbers)); //array of methods
 console.log(numbers);
 
 function hello(){
     console.log("hello");
 }
+
 // prototype
-console.log(hello)
+console.log(hello.prototype); // it is an object
+
+hello.prototype = []; //type of prototype changed 
+console.log(hello.prototype)
+hello.prototype.push('2');
